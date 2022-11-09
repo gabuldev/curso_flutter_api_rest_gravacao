cff5bd9a5419db71f6dc7c9bae66f27383a74f3c
Gabul DEV <contato@gabul.dev>
2022-11-09 09:38:06 -0300

    add custom dio and log interceptor

--- lib/features/start/start_repository.dart
+++ lib/features/start/start_repository.dart
@@ -1,6 +1,5 @@
-import 'package:dio/dio.dart';
+import 'package:flutter_api_rest/shared/services/custom_dio.dart';
 
-import '../../shared/models/author.dart';
 import '../../shared/models/post.dart';
 
 abstract class StartRepository {
@@ -8,27 +7,13 @@ abstract class StartRepository {
 }
 
 class StartRepositoryImpl implements StartRepository {
-  final client = Dio();
+  final client = CustomDio();
   @override
   Future<List<Post>> getPosts() async {
     try {
-      final response =
-          await client.get("https://jsonplaceholder.typicode.com/posts");
+      final response = await client.get("/photos");
       final data = response.data as List;
-      return data
-          .map(
-            (e) => Post(
-                title: e['title'].toString().substring(0, 5),
-                tags: ["Android"],
-                readTime: "5 min",
-                photoUrl: "https://picsum.photos/1920/1080",
-                hasReadLater: false,
-                author: Author(
-                    id: "id",
-                    name: "Augusto",
-                    profileUrl: "https://picsum.photos/100/100")),
-          )
-          .toList();
+      return data.map((e) => Post.fromMap(e)).toList();
     } catch (e) {
       throw Exception("Nao foi possivel buscar os dados");
     }
---
+++ lib/shared/services/custom_dio.dart
@@ -0,0 +1,11 @@
+import 'package:dio/native_imp.dart';
+import 'package:flutter_api_rest/shared/services/interceptors/log_interceptor.dart';
+import 'package:flutter_api_rest/shared/services/interceptors/post_interceptor.dart';
+
+class CustomDio extends DioForNative {
+  CustomDio() {
+    options.baseUrl = "https://jsonplaceholder.typicode.com";
+    interceptors.add(LogInterceptor());
+    interceptors.add(PostInterceptor());
+  }
+}
